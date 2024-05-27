c1c48b55b5e31fa6eeb3a41d283fa28623bf36e3
Jiri Sindelar <jorge.sindelar@gmail.com>
2024-05-27 17:14:48 +0200

    Client - footer

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../navBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "../footer/Footer";
+import '../../assets/css/layout/Layout.css';
 
 function Layout() {
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <div className="sticky-top">
+        <NavBar />
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+      <div className="sticky-footer">
+        <Footer />
+      </div>
     </>
   );
 }
